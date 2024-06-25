ea1fe69571f52fe2a2ba2d40cb0a906e977a376e
tanzeelak <tnkhan8042@gmail.com>
2024-06-24 20:58:59 -0700

    yay

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 install:
 	pip3 install -r requirements.txt
 
-run:
+run-baddie:
 	python3 -m src.baddie
+
+run-life:
+	python3 -m src.life
 
 test:
 	pytest && rm -rf .pytest_cache
--- src/life.py
+++ src/life.py
@@ -4,4 +4,18 @@ from .baddie import Baddie
 class Life:
     def __init__(self) -> None:
         self.baddies: List[Baddie] = []
-        
+    
+    def configure_life(self) -> None:
+        print("baddies r an important part of life")
+        # take all the data from data.baddies.txt and initialize a list of baddies here.f
+        # get list of baddies from data.baddies.txt
+        for line in open("data/baddies.txt"):
+            params = line.split(",")
+            self.baddies.append(Baddie(params[0], params[1], params[2]))
+        print(self.baddies)
+
+    def main():
+        print("YISHAW")
+
+    if __name__ == "__main__":
+        main()
